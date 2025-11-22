4277d9a0f40c167de6612270365e72a788d48573
ddamaja <staystrongbg@gmail.com>
2025-11-22 19:15:49 +0100

    removed baseUrl from auth on the server side

--- lib/auth.ts
+++ lib/auth.ts
@@ -9,7 +9,6 @@ import {
 } from "@/constants";
 
 export const auth = betterAuth({
-  baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000",
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
