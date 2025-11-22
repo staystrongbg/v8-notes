3d0fff4294b99c7301a275299969596cf78ffa6d
ddamaja <staystrongbg@gmail.com>
2025-11-22 15:11:37 +0100

    base url change 8

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,7 +2,6 @@ import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "@/lib/prisma";
 import {
-  betterAuthSecret,
   githubClientId,
   githubClientSecret,
   googleClientId,
@@ -13,9 +12,6 @@ export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
-  baseUrl: "https://v8-notes.vercel.app",
-  secret: betterAuthSecret,
-  trustedOrigins: ["https://v8-notes.vercel.app", "http://localhost:3000"],
   emailAndPassword: {
     enabled: true,
   },
@@ -34,6 +30,7 @@ export const auth = betterAuth({
       enabled: true,
     },
   },
+  trustedOrigins: ["https://v8-notes.vercel.app", "http://localhost:3000"],
 });
 
 //TODO add confirmation email when signing up
