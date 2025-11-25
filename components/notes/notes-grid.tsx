0606464f3a305fcd05a1ad9ee7f55d752ef17d92
ddamaja <staystrongbg@gmail.com>
2025-11-25 23:22:17 +0100

    xs viewport snaping implemented

--- components/notes/notes-grid.tsx
+++ components/notes/notes-grid.tsx
@@ -11,7 +11,7 @@ interface NotesGridProps {
 export const NotesGrid = ({ promise }: NotesGridProps) => {
   const notes = use(promise);
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 h-screen sm:h-auto overflow-y-auto snap-y snap-mandatory sm:snap-none">
       <NoteCard notes={notes} />
     </div>
   );
