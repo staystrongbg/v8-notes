3c2579a05c23c1349bc02018819800964e6287f8
ddamaja <staystrongbg@gmail.com>
2025-12-02 08:33:12 +0100

    refactor session check

--- lib/require-user-session.ts
+++ lib/require-user-session.ts
@@ -1,15 +1,18 @@
 import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 import { unauthorized } from "next/navigation";
+import { cache } from "react";
 
-export const requireUserSession = async () => {
+export const requireUserSession = cache(async () => {
   const session = await auth.api.getSession({
     headers: await headers(),
   });
+
+  console.log("session", session);
 
   if (!session?.user) {
     return unauthorized();
   }
 
   return session;
-};
+});
